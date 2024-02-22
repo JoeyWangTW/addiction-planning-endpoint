44483e3678151512af167a2569299fea6cd5e888
Joey Wang <joey840404@gmail.com>
2024-02-22 01:33:22 -0800

    allow cors

--- llm_api/main.py
+++ llm_api/main.py
@@ -1,7 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
